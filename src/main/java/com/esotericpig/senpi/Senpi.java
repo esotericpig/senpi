be2e0b9f689fc43d3adc9b4b4f9956716c04d992
esotericpig <esotericpig@users.noreply.github.com>
2016-01-10 22:28:23 +0900

    Initial commit.

---
+++ src/main/java/com/esotericpig/senpi/Senpi.java
@@ -0,0 +1,10 @@
+package com.esotericpig.senpi;
+
+/**
+ * @author Jonathan Bradley Whited, @esotericpig
+ */
+public class Senpi {
+  public static void main(String[] args) {
+    BigIntBase.main(args);
+  }
+}
---
+++ src/test/java/com/esotericpig/senpi/AllBigIntBaseTest.java
@@ -0,0 +1,145 @@
+package com.esotericpig.senpi;
+
+import java.math.BigInteger;
+import java.util.Random;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author Jonathan Bradley Whited, @esotericpig
+ */
+public class AllBigIntBaseTest {
+  public static final int TEST_COUNT = 100; // Number of tests to perform
+
+  protected Random rand = null;
+  
+  @Before
+  public void setUp() {
+    rand = new Random();
+  }
+
+  public static String createRandNumStr(int base,int minLen,int maxLen,boolean allowZeroPad,Random rand) {
+    if(base < 2) {
+      // Avoid infinite loop at !allowZeroPad
+      throw new RuntimeException("Invalid base.");
+    }
+  
+    int digit = 0;
+    int len = minLen + rand.nextInt(maxLen);
+    StringBuilder sb = new StringBuilder(len);
+    
+    // -#? (-0 is allowed)
+    if(rand.nextBoolean()) {
+      sb.append("-");
+    }
+    // Add "+" or not for +#? (+0 is allowed)
+    else if(rand.nextBoolean()) {
+      sb.append("+");
+    }
+    // 007?
+    if(!allowZeroPad) {
+      while((digit = rand.nextInt(base)) == 0) {
+      }
+      sb.append(Integer.toString(digit,base));
+    }
+    for(int i = 0; i < len; ++i) {
+      digit = rand.nextInt(base);
+      sb.append(Integer.toString(digit,base));
+    }
+    return sb.toString();
+  }
+
+  @Test
+  public void testAll() {
+    // Currently, only tests:
+    //   - +, -, *, /, % (modulus), r (remainder)
+    //   - Base 12
+    // (Called modulus because it is the absolute value, instead of modulo;
+    //  however, it is kind of ambiguous.)
+    final int base = 12;
+    final char[] ops = {'+','-','*','/','%','r'};
+    
+    StringBuilder msg = new StringBuilder();
+    
+    for(char op: ops) {
+      for(int t = 0; t < TEST_COUNT; ++t) {
+        String numStrA = createRandNumStr(base,1,100,rand.nextBoolean(),rand);
+        String numStrB = createRandNumStr(base,1,100,rand.nextBoolean(),rand);
+        
+        MutBigIntBase ma = new MutBigIntBase(numStrA,base);
+        MutBigIntBase mb = new MutBigIntBase(numStrB,base);
+        
+        BigIntBase ba = new BigIntBase(numStrA,base);
+        BigIntBase bb = new BigIntBase(numStrB,base);
+        BigIntBase bc = null;
+        
+        BigInteger a = new BigInteger(numStrA,base);
+        BigInteger b = new BigInteger(numStrB,base);
+        BigInteger c = null;
+        
+        switch(op) {
+          case '+': ma.plus(mb); bc = ba.plus(bb); c = a.add(b); break;
+          case '-': ma.minus(mb); bc = ba.minus(bb); c = a.subtract(b); break;
+          case '*': ma.times(mb); bc = ba.times(bb); c = a.multiply(b); break;
+          case '/':
+            if(b.signum() == 0) {
+              mb = new MutBigIntBase("1",base);
+              bb = new BigIntBase("1",base);
+              b = new BigInteger("1",base);
+            }
+            ma.over(mb); bc = ba.over(bb); c = a.divide(b);
+            break;
+          case '%':
+            if(b.signum() == 0) {
+              mb = new MutBigIntBase("1",base);
+              bb = new BigIntBase("1",base);
+              b = new BigInteger("1",base);
+            }
+            if(a.signum() == -1 || b.signum() == -1) {
+              // Both #s must be positive because of differences in BigInteger
+              ma.abs();
+              mb.abs();
+              ba = ba.abs();
+              bb = bb.abs();
+              a = a.abs();
+              b = b.abs();
+            }
+            ma.mod(mb); bc = ba.mod(bb); c = a.mod(b);
+            break;
+          case 'r':
+            if(b.signum() == 0) {
+              mb = new MutBigIntBase("1",base);
+              bb = new BigIntBase("1",base);
+              b = new BigInteger("1",base);
+            }
+            ma.rem(mb); bc = ba.rem(bb); c = a.remainder(b);
+            break;
+          default: assertNull("Operation undefined: " + c,null);
+        }
+        
+        String mcs = ma.toString().toUpperCase();
+        String bcs = bc.toString().toUpperCase();
+        String as = a.toString(base).toUpperCase();
+        String bs = b.toString(base).toUpperCase();
+        String cs = c.toString(base).toUpperCase();
+        
+        msg.setLength(0);
+        msg.append("\n");
+        msg.append("MutBigIntBase:\n");
+        msg.append("\t" + as + " " + op + " " + mb + " = " + mcs + "\n");
+        msg.append("BigIntBase:\n");
+        msg.append("\t" + ba + " " + op + " " + bb + " = " + bcs + "\n");
+        msg.append("BigInteger:\n");
+        msg.append("\t" + as + " " + op + " " + bs + " = " + cs + "\n");
+        msg.append("\t" + a + " " + op + " " + b + " = " + c + "\n"); // Base 10 (decimal)
+        String m = msg.toString();
+        
+        assertEquals(m,mcs,cs);
+        assertEquals(m,bcs,cs);
+      }
+    }
+  }
+}
