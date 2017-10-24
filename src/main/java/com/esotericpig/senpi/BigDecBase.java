8dfb61b8d855838f093275dbe9f14816108876ae
esotericpig <esotericpig@users.noreply.github.com>
2017-10-24 19:13:14 +0900

    Add license to each file

--- src/main/java/com/esotericpig/senpi/BigDecBase.java
+++ src/main/java/com/esotericpig/senpi/BigDecBase.java
@@ -1,7 +1,29 @@
+/**
+ * This file is part of senpi.
+ * Copyright (c) 2016-2017 Jonathan Bradley Whited (@esotericpig)
+ * 
+ * senpi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * senpi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with senpi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.esotericpig.senpi;
 
 import java.io.Serializable;
 
+// TODO: plus, minus, times, over
+// TODO: abs, compareTo, negate
+// TODO: cache, sqrt, pow
+// TODO: Goal: 1/3=0.4 (instead of 0.333...); 1/6=0.2; 1/9=0.14
 /**
  * @author Jonathan Bradley Whited, @esotericpig
  */
@@ -10,6 +32,11 @@ public class BigDecBase implements Serializable {
   
   protected int scale = 0;
   protected MutBigIntBase value = null;
+  
+  public BigDecBase(BigDecBase bdb) {
+    this.scale = bdb.scale;
+    this.value = new MutBigIntBase(bdb.value);
+  }
   
   public BigDecBase(String s) {
     this(s,MutBigIntBase.DEFAULT_BASE);
@@ -24,6 +51,10 @@ public class BigDecBase implements Serializable {
     
     this.scale = pd.scale;
     this.value = new MutBigIntBase(pd.sign,pd.digits,base,pd.offset,pd.length);
+  }
+  
+  public BigDecBase plus(BigDecBase bdb) {
+    return null;
   }
   
   public String toString() {
--- src/main/java/com/esotericpig/senpi/Senpi.java
+++ src/main/java/com/esotericpig/senpi/Senpi.java
@@ -1,10 +1,39 @@
+/**
+ * This file is part of senpi.
+ * Copyright (c) 2016-2017 Jonathan Bradley Whited (@esotericpig)
+ * 
+ * senpi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * senpi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with senpi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.esotericpig.senpi;
 
+// TODO: when calculating pi, use base 10 to ensure algorithm is working
+//       and then use base 12, as the number may be different than base 12
+//       numbers on the internet, due to actually using a proper representation
 /**
  * @author Jonathan Bradley Whited, @esotericpig
  */
 public class Senpi {
   public static void main(String[] args) {
-    BigIntBase.main(args);
+    //BigIntBase.main(args);
+    
+    //123.456
+    //BigDecBase a = new BigDecBase("000000123AB.456AB",12);
+    //System.out.println(a);
+    
+    BigDecBase a = new BigDecBase("1A.1A");
+    BigDecBase b = new BigDecBase("1B.1B");
+    System.out.println(a.plus(b));
   }
 }
